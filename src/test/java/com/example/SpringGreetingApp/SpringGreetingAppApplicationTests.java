f324b28ad14fedfd717072fecc4c8a75fe2ef135
Mudit0208 <mudit.jadonn@gmail.com>
2025-03-08 11:17:53 +0530

    [Mudit] Added UC1 assignment

---
+++ src/test/java/com/example/SpringGreetingApp/SpringGreetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringGreetingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGreetingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
